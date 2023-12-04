3383785d04a18a937b54d2a5388bb7b289af9833
PSY <sksssd234@naver.com>
2023-12-04 20:57:09 +0900

    start project

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+// prettier.config.js
+module.exports = {
+  singleQuote: true,
+  trailingComma: "all",
+  printWidth: 120,
+  tabWidth: 4,
+  useTabs: false,
+  semi: true,
+  bracketSpacing: true,
+  endOfLine: "auto",
+  arrowParens: "always",
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindcss: "",
+};
