01387e073b5cc0250329343315fe4aa47b98afdc
PSY <sksssd234@naver.com>
2023-12-10 20:15:43 +0900

    cover

--- components/Home.js
+++ components/Home.js
@@ -1,29 +1,73 @@
-import { useState } from "react";
-import Item from "./Item";
+import { useEffect, useState } from 'react';
+import Item from './Item';
+import Module from './ModuleComponent';
+import ErrorComponent from './Error';
+
+let moduleCount = 0;
 
 const Home = () => {
-  const [type, setType] = useState({ type: "", count: null });
-  const clickHandler = (item) => {
-    setType(item);
-  };
-  let title = "다음 콘텐츠들 중 하나를 선택하세요";
-  if (type.count === 1) {
-    title = `${type.type}와 관련된 것 중에서 선택해 주세요`;
-  } else if (type.count === 2) {
-    title = `${type.type}에서 선택하세요`;
-  } else if (type.count >= 3) {
-    title = `그냥 이걸 보세요`;
-  }
-  return (
-    <div className="mt-8 space-y-4 sm:mx-auto md:w-1/2 ">
-      <div className="bg-red-300 p-8 text-center">
-        <h1>{title}</h1>
-      </div>
-      <div className="grid grid-cols-3 grid-rows-3 gap-4  p-4">
-        <Item onTitle={clickHandler} />
-      </div>
-    </div>
-  );
+    const [type, setType] = useState({ type: '', count: null });
+    const clickHandler = (item) => {
+        setType(item);
+    };
+    const [isModule, setIsModule] = useState(false);
+    const [isError, setIsError] = useState(false);
+    let w = null;
+    let t = null;
+    let c = null;
+
+    let title = '다음 콘텐츠들 중 하나를 선택하세요';
+
+    useEffect(() => {
+        if (moduleCount === 0) {
+            moduleCount++;
+            return;
+        }
+        if (type.count >= 4) {
+            setTimeout(() => {
+                setIsError(true);
+            }, 1000);
+            return;
+        }
+        setIsModule(true);
+        setTimeout(() => {
+            setIsModule(false);
+        }, 4000);
+    }, [type.count]);
+
+    if (type.count === 1) {
+        w = 'w-1/3';
+        t = '1단계';
+        c = 'bg-orange-400';
+    } else if (type.count === 2) {
+        w = 'w-2/3';
+        t = '2단계';
+        c = 'bg-lime-400 ';
+    } else if (type.count === 3) {
+        w = 'w-full';
+        t = '3단계';
+        c = 'bg-indigo-400 ';
+        title = '너 이거 좋아하지? 이거 봐';
+    } else if (type.count >= 4) {
+        title = '내가 보라는걸 보세요';
+    }
+
+    return (
+        <div className=" mt-8 space-y-4 sm:mx-auto md:w-1/2">
+            {isError ? <ErrorComponent /> : null}
+            {isModule ? (
+                <Module width={w} color={c}>
+                    {t}
+                </Module>
+            ) : null}
+            <div className="bg-red-300 p-8 text-center">
+                <h1>{title}</h1>
+            </div>
+            <div className="grid grid-cols-3 grid-rows-3 gap-4  p-4">
+                <Item onTitle={clickHandler} />
+            </div>
+        </div>
+    );
 };
 
 export default Home;
--- components/module.js
+++
@@ -1,5 +0,0 @@
-const module = ({ children, className }) => {
-  return <div className={`${className} absolute`}>{children}</div>;
-};
-
-export default module;
